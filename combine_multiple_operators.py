15b9d473acc68c9619f49eff9d2036d721109e79
Abdul Raheem <alimuaviashah100010@gmail.com>
2026-03-09 11:27:22 +0500

    Refactor condition for outdoor activities message

--- combine_multiple_operators.py
+++ combine_multiple_operators.py
@@ -1,1 +1,18 @@
+temperature = 25
+raining = False
+weekend = True
 
+# if (temperature > 20 and not raining) or weekend :
+#     print("Great day for out activities")
+# else :
+#     print("not good day for activities")
+
+if temperature > 20 and not raining and (weekend or not weekend):
+    print("Great day for outdoor activities")
+else:
+    print("Not a great day for activities")
+    
+
+ 
+
+    
